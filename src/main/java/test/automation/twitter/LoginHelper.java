5e621899077948adbf0e61b7a44d095c3111649c
Ririn Nurmaica <nurmaica@gmail.com>
2020-08-03 19:29:29 +0700

    test(login): update import assertEquals

--- src/main/java/test/automation/twitter/LoginHelper.java
+++ src/main/java/test/automation/twitter/LoginHelper.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static org.junit.Assert.assertEquals;
-
 public class LoginHelper {
     WebDriver driver;
 
